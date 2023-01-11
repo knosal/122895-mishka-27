6bfd75778c3518518a55f538e9673862287f4852
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-01-11 13:25:51 +0300

    Добавил файл js. Сделал кнопку в меню через тени

---
+++ source/js/script.js
@@ -0,0 +1,15 @@
+let navMain = document.querySelector(".main-navigation");
+let navToggle = document.querySelector(".main-navigation__toggle");
+
+navMain.classList.remove("main-navigation--nojs");
+
+navToggle.addEventListener("click", function () {
+  //navToggle.classList.toogle("main-nav--closed")
+ if (navMain.classList.contains("main-navigation--closed")) {
+    navMain.classList.remove("main-navigation--closed");
+    navMain.classList.add("main-navigation--opened");
+  } else {
+    navMain.classList.add("main-navigation--closed");
+    navMain.classList.remove("main-navigation--opened");
+  }
+});

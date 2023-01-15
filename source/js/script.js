eca26e1bcdb343b9bb1eb71bd17f635e4d303b0e
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-01-15 15:10:05 +0300

    Сделал js модальных окон. Исправил ошибки

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,43 @@
-let navMain = document.querySelector(".main-navigation");
-let navToggle = document.querySelector(".main-navigation__toggle");
+// -------- Кнопка меню-бургер
+let navigationMain = document.querySelector(".main-navigation");
+let navigationSwitch = document.querySelector(".main-navigation__toogle");
 
-navMain.classList.remove("main-navigation--nojs");
+navigationMain.classList.remove("main-navigation--nojs");
 
-navToggle.addEventListener("click", function () {
-  //navToggle.classList.toogle("main-nav--closed")
-  if (navMain.classList.contains("main-navigation--closed")) {
-    navMain.classList.remove("main-navigation--closed");
-    navMain.classList.add("main-navigation--opened");
+navigationSwitch.addEventListener("click", function () {
+  if (navigationMain.classList.contains("main-navigation--closed")) {
+    navigationMain.classList.remove("main-navigation--closed");
+    navigationMain.classList.add("main-navigation--opened");
   } else {
-    navMain.classList.add("main-navigation--closed");
-    navMain.classList.remove("main-navigation--opened");
+    navigationMain.classList.add("main-navigation--closed");
+    navigationMain.classList.remove("main-navigation--opened");
   }
 });
+
+// -------- Кнопка в корзину. Модалка (catalog.html)
+let catalogBuyButton = document.querySelectorAll(".product__button-buy");
+let catalogModal = document.querySelector(".modal--close");
+
+
+for (let catalogBuyButtons of catalogBuyButton) {
+  catalogBuyButtons.onclick = function () {
+    catalogModal.classList.toggle("modal--close");
+  };
+}
+let modalCloseButton = document.querySelector(".modal__button");
+
+modalCloseButton.onclick = function () {
+  catalogModal.classList.add("modal--close");
+};
+
+// -------- Кнопка в корзину. Модалка (index.html)
+let featuredBuyButton = document.querySelector(".featured__button");
+let indexModal = document.querySelector(".modal--close");
+
+featuredBuyButton.onclick = function () {
+  indexModal.classList.toggle("modal--close");
+}
+
+modalCloseButton.onclick = function () {
+  indexModal.classList.add("modal--close");
+};

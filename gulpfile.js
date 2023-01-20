b412e13033305c835d4e0ad59c7ba8e7fd38e976
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-01-20 16:40:32 +0300

    Min css, rename, создание build

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
-import gulp from 'gulp';
-import plumber from 'gulp-plumber';
-import sass from 'gulp-dart-sass';
-import postcss from 'gulp-postcss';
-import autoprefixer from 'autoprefixer';
-import browser from 'browser-sync';
+import gulp from 'gulp'; //// Gulp для автоматизации
+import plumber from 'gulp-plumber'; // перехват ошибок, и после устранения ошибки сборка восстановит работоспособность
+import sass from 'gulp-dart-sass'; // для использования препроцессора SASS
+import postcss from 'gulp-postcss'; //
+import autoprefixer from 'autoprefixer'; // Автопрефиксы
+import csso from 'postcss-csso'; //минимизатор CSS
+import rename from 'gulp-rename'; // // переименование файлов
+import browser from 'browser-sync'; //
 
 // Styles
 
@@ -12,14 +14,27 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
 
-// Server
+// Html
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(gulp.dest('build'));
+}
 
+// Scripts
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(gulp.dest('build/js'));
+}
+
+// Server
 const server = (done) => {
   browser.init({
     server: {
@@ -33,12 +48,10 @@ const server = (done) => {
 }
 
 // Watcher
-
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
-
 
 export default gulp.series(
   styles, server, watcher
